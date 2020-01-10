da1aa31dd854d3538358d06ab46f55bc148c1fe4
moncef <team922i@gmail.com>
2020-01-10 17:58:37 +0100

    reglage du menu navbar f hydra m3a si taher

--- resources/views/layouts/cadre.blade.php
+++ resources/views/layouts/cadre.blade.php
@@ -57,44 +57,47 @@
                             <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
                         </li>
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle " href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Explore <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+                            <a class="nav-link dropdown-toggle " href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Services <i class="fa fa-angle-down" aria-hidden="true"></i></a>
                             <div class="dropdown-menu dropdown-secondary " aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="index.html">Home</a>
-                                <a class="dropdown-item" href="explore.html">Explore</a>
-                                <a class="dropdown-item" href="listing.html">Listing</a>
-                                <a class="dropdown-item" href="single-listing.html">Single Listing</a>
-                                <a class="dropdown-item" href="contact.html">Contact</a>
+                                <a class="dropdown-item" href="index.html">Demande</a>
+                                <a class="dropdown-item" href="explore.html">Proposition</a>
+
                             </div>
                         </li>
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown2" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Listings <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown2" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Bénévolats  <i class="fa fa-angle-down" aria-hidden="true"></i></a>
                             <div class="dropdown-menu" aria-labelledby="navbarDropdown2">
-                                <a class="dropdown-item" href="index.html">Home</a>
-                                <a class="dropdown-item" href="explore.html">Explore</a>
-                                <a class="dropdown-item" href="listing.html">Listing</a>
-                                <a class="dropdown-item" href="single-listing.html">Single Listing</a>
-                                <a class="dropdown-item" href="contact.html">Contact</a>
+                                <a class="dropdown-item" href="index.html">Demande</a>
+                                <a class="dropdown-item" href="explore.html">Proposition</a>
                             </div>
                         </li>
                         <li class="nav-item">
+                            <a class="nav-link" href="contact.html">Activités Association</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="contact.html">Services Professionnels</a>
+                        </li>
+
+
+                        <li class="nav-item">
                             <a class="nav-link" href="contact.html">Contact</a>
                         </li>
+
+
+
                     </ul>
-                    <!-- Search btn -->
-                    <div class="dorne-search-btn">
-                        <a id="search-btn" href="#"><i class="fa fa-search" aria-hidden="true"></i> Search</a>
-                    </div>
-                    <!-- Signin btn -->
+
 
                     @guest
 
                     <div class="dorne-signin-btn">
-                        <a href="{{ route('login') }}">Sign in </a>
+                        <a href="{{ route('login') }}">Connexion</a>
 
                     </div>
                             @if (Route::has('register'))
                     <div class="dorne-signin-btn">
-                        <a href="{{ route('register') }}">Register</a>
+                        <a href="{{ route('register') }}">Inscription</a>
                     </div>
                         @endif
 
